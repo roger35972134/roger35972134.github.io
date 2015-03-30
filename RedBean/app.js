c4c1c5b1f5db14c4a3a5fd0fdbd3849b496682f1
Roger Wang <roger35972134@gmail.com>
2015-03-30 16:25:51 +0800

    redbean ver1.1

--- RedBean/app.js
+++ RedBean/app.js
@@ -6,6 +6,9 @@ var main=function(){
 		$(this).css('color','white')
 	});
 	$('.pie a').on('click',function(){
+		$(this).css('color','blue');
+		$('.chicken a').css('color','white');
+		$('.map a').css('color','white');
 		$('.RedBean').animate({
 			top:'50px'
 		},200);
@@ -17,6 +20,9 @@ var main=function(){
 		},200);
 	});
 	$('.chicken a').on('click',function(){
+		$(this).css('color','blue');
+		$('.pie a').css('color','white');
+		$('.map a').css('color','white');
 		$('.RedBean').animate({
 			top:'-1000px'
 		},200);
@@ -28,6 +34,9 @@ var main=function(){
 		},200);
 	});
 	$('.map a').on('click',function(){
+		$(this).css('color','blue');
+		$('.pie a').css('color','white');
+		$('.chicken a').css('color','white');
 		$('.RedBean').animate({
 			top:'-1000px'
 		},200);
