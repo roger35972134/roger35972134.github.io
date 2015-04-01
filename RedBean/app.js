d6143041b4611bb60fa64ad1167909268c13d009
Roger Wang <roger35972134@gmail.com>
2015-04-02 01:18:37 +0800

    redbean ver1.3

--- RedBean/app.js
+++ RedBean/app.js
@@ -1,6 +1,6 @@
 var main=function(){
 	$('.table td').on('mouseover',function(){
-		$(this).css('color','black')
+		$(this).css('color','blue')
 	});
 	$('.table td').on('mouseout',function(){
 		$(this).css('color','white')
@@ -10,6 +10,7 @@ var main=function(){
 		$('.chicken a').css('color','white');
 		$('.map a').css('color','white');
 		$('.telephone a').css('color','white');
+		$('.picture a').css('color','white');
 		$('.RedBean').animate({
 			top:'50px'
 		},200);
@@ -22,12 +23,16 @@ var main=function(){
 		$('.Telephone').animate({
 			top:'-1000px'
 		},200);
+		$('.list').animate({
+			top:'-10000px'
+		},1000);
 	});
 	$('.chicken a').on('click',function(){
 		$(this).css('color','blue');
 		$('.pie a').css('color','white');
 		$('.map a').css('color','white');
 		$('.telephone a').css('color','white');
+		$('.picture a').css('color','white');
 		$('.RedBean').animate({
 			top:'-1000px'
 		},200);
@@ -40,12 +45,16 @@ var main=function(){
 		$('.Telephone').animate({
 			top:'-1000px'
 		},200);
+		$('.list').animate({
+			top:'-10000px'
+		},1000);
 	});
 	$('.map a').on('click',function(){
 		$(this).css('color','blue');
 		$('.pie a').css('color','white');
 		$('.chicken a').css('color','white');
 		$('.telephone a').css('color','white');
+		$('.picture a').css('color','white');
 		$('.RedBean').animate({
 			top:'-1000px'
 		},200);
@@ -58,12 +67,16 @@ var main=function(){
 		$('.Telephone').animate({
 			top:'-1000px'
 		},200);
+		$('.list').animate({
+			top:'-10000px'
+		},1000);
 	});
 	$('.telephone a').on('click',function(){
 		$(this).css('color','blue');
 		$('.pie a').css('color','white');
 		$('.chicken a').css('color','white');
 		$('.map a').css('color','white');
+		$('.picture a').css('color','white');
 		$('.RedBean').animate({
 			top:'-1000px'
 		},200);
@@ -76,6 +89,31 @@ var main=function(){
 		$('.Telephone').animate({
 			top:'150px'
 		},200);
+		$('.list').animate({
+			top:'-10000px'
+		},1000);
+	});
+	$('.picture a').on('click',function(){
+		$(this).css('color','blue');
+		$('.chicken a').css('color','white');
+		$('.map a').css('color','white');
+		$('.telephone a').css('color','white');
+		$('.pie a').css('color','white');
+		$('.RedBean').animate({
+			top:'-1000px'
+		},200);
+		$('.Chicken').animate({
+			top:'-1000px'
+		},200);
+		$('#googleMap').animate({
+			top:'-1000px'
+		},200);
+		$('.Telephone').animate({
+			top:'-1000px'
+		},200);
+		$('.list').animate({
+			top:'0px'
+		},1000);
 	});
 };
 
